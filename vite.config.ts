cabf85d9cbfb37f5e384d6baf2ca9797b5db73f9
KritamateGunt <Kritamate.bs@gmail.com>
2025-11-02 23:08:44 +0700

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/q-a_thai_mp/',   // ชื่อ repo ของคุณ
+  base: '/',   // ชื่อ repo ของคุณ
   plugins: [react()],
 })
